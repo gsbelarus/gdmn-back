157c6d70898c0ac86e3c38502f236ecddd58a2b9
Andrej Kirejeŭ <gs1994@gmail.com>
2018-03-13 17:26:27 +0300

    wip

--- src/db/atDatabase.ts
+++ src/db/atDatabase.ts
@@ -1,17 +1,15 @@
-export class SQLDatabase {
+export class RDatabase {
   private relations: Relation[] = [];
-
-  loadFromDatabase(
 }
 
-export class Relation {  
-  constructor (public readonly relationName: string) {}
+export class Relation {
+  constructor(public readonly relationName: string) {}
 }
 
 export class Field {
-  constructor (public readonly fieldName: string) {}
+  constructor(public readonly fieldName: string) {}
 }
 
 export class RelationField {
-  constructor (public readonly fieldName: string) {}
+  constructor(public readonly fieldName: string) {}
 }
