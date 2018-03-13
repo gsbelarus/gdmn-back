69d6f5393dea7782a32c1166d613e22b35f3bb08
Andrej Kirejeŭ <gs1994@gmail.com>
2018-03-13 16:56:37 +0300

    removing submodule

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,30 +1,41 @@
-import { attach, Database, DatabaseCallback, Options } from 'node-firebird';
+import { attach, Database, DatabaseCallback, ISOLATION_READ_COMMITED_READ_ONLY,
+  Options, Transaction } from '../../lib/node-firebird';
 import opt from './connection_options';
 
 let db: Database;
+let readTransaction: Transaction;
 
 export function connect(alias = '') {
   if (db) {
-    throw new Error('db already attached');
+    throw new Error('db is already connected');
   }
   attach(opt(), (err, connectedDB) => {
     if (err) {
       throw new Error(err);
     }
     db = connectedDB;
+    db.transaction(ISOLATION_READ_COMMITED_READ_ONLY, (trErr, tr) => {
+      if (trErr) {
+        throw new Error(trErr);
+      }
+      readTransaction = tr;
+    });
   });
 }
 
 export function disconnect() {
   if (!db) {
-    throw new Error('db has not been connected');
+    throw new Error('db is not connected');
+  }
+  if (readTransaction) {
+    readTransaction.commit();
   }
   db.detach();
 }
 
 export function getDB() {
   if (!db) {
-    throw new Error('db has not been connected');
+    throw new Error('db is not connected');
   }
   return db;
 }
