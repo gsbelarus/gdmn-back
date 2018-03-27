ba75fe45eaa35c4a17393a59f3214a55c4fec74c
sywka1 <lomashko.yuri@gmail.com>
2018-03-27 17:54:52 +0300

    update gdmn-db

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,16 @@ import databases, {IDBAlias} from "./db/databases";
 async function init({poolInstance, max, options}: IDBAlias<any>) {
     await poolInstance.create(options, max);
     const dbStructure = await ADatabase.executeTransactionPool(poolInstance,
-        transaction => transaction.readDBStructure());
+        async transaction => {
+
+            const resultSet = await transaction.executeSQL("SELECT * FROM GD_DOCUMENT");
+            await resultSet.to(1);
+            while (await resultSet.previous()) {
+                console.log(resultSet.getObject());
+            }
+
+            return await transaction.readDBStructure();
+        });
 
     console.log(dbStructure);
 }
