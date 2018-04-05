6c3a2d0c20a68ea0ad0aaec2cf8050d2c73f5c2b
sywka1 <lomashko.yuri@gmail.com>
2018-04-05 13:23:05 +0300

    update libs and tslint.json

--- src/db/databases.ts
+++ src/db/databases.ts
@@ -1,29 +1,31 @@
-import {DefaultConnectionPoolOptions, Factory, TConnectionPool, TDBOptions} from "gdmn-db";
+import {Factory, IDBOptions, IDefaultConnectionPoolOptions, TConnectionPool} from "gdmn-db";
 
-export type TDB = { [alias: string]: IDBAlias<any> };
-
-export interface IDBAlias<Options> {
-    alias: string;
-    dbOptions: TDBOptions;
-    options: Options,
-    poolInstance: TConnectionPool<Options>;
+export interface IDB {
+  [alias: string]: IDBAlias<any>;
 }
 
-const databases: TDB = {
-    broiler: <IDBAlias<DefaultConnectionPoolOptions>>{
-        alias: "broiler",
-        poolInstance: Factory.FBModule.newDefaultConnectionPool(),
-        options: {
-            max: 3
-        },
-        dbOptions: {
-            host: "brutto",
-            port: 3053,
-            username: "SYSDBA",
-            password: "masterkey",
-            path: "k:\\bases\\broiler\\GDBASE_2017_10_02.FDB"
-        }
+export interface IDBAlias<Options> {
+  alias: string;
+  dbOptions: IDBOptions;
+  options: Options;
+  poolInstance: TConnectionPool<Options>;
+}
+
+const databases: IDB = {
+  broiler: {
+    alias: "broiler",
+    poolInstance: Factory.FBModule.newDefaultConnectionPool(),
+    options: {
+      max: 3
+    },
+    dbOptions: {
+      host: "brutto",
+      port: 3053,
+      username: "SYSDBA",
+      password: "masterkey",
+      path: "k:\\bases\\broiler\\GDBASE_2017_10_02.FDB"
     }
+  } as IDBAlias<IDefaultConnectionPoolOptions>
 };
 
 export default databases;
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
-import {GraphQLServer} from "graphql-yoga";
 import {AConnectionPool, ADatabase, ATransaction} from "gdmn-db";
-import databases, {IDBAlias} from "./db/databases";
 import {erExport, ERModel} from "gdmn-orm";
+import {GraphQLServer} from "graphql-yoga";
+import databases, {IDBAlias} from "./db/databases";
 
 const erModel = new ERModel();
 
-async function init({ poolInstance, options, dbOptions }: IDBAlias<any>) {
+async function init({poolInstance, options, dbOptions}: IDBAlias<any>) {
   await poolInstance.create(dbOptions, options);
 
   return await AConnectionPool.executeDatabase(poolInstance,
-    database => ADatabase.executeTransaction(database, async transaction => {
+    (database) => ADatabase.executeTransaction(database, async (transaction) => {
       // example
       await ATransaction.executeResultSet(transaction, "SELECT * FROM GD_DOCUMENT", null,
-        async resultSet => {
+        async (resultSet) => {
           await resultSet.to(1);
           while (await resultSet.previous()) {
             console.log(resultSet.getArray());
           }
         });
 
-        return await transaction.readDBStructure();
+      return await transaction.readDBStructure();
     }));
 }
 
 init(databases.broiler)
-.then( dbs => erExport(dbs, erModel) )
-.then( erm => console.log(erm) )
-.catch(console.warn);
+  .then((dbs) => erExport(dbs, erModel))
+  .then((erm) => console.log(erm))
+  .catch(console.warn);
 
 const typeDefs = `
   type Query {
@@ -43,6 +43,6 @@ const resolvers = {
 const server = new GraphQLServer({typeDefs, resolvers});
 
 server.express.get("/hello", (req, res) => res.send("Hello World!"));
-server.express.get("/er", (req, res) => res.send(erModel) );
+server.express.get("/er", (req, res) => res.send(erModel));
 
 server.start(() => console.log("Server is running on localhost:4000")).catch(console.error);
