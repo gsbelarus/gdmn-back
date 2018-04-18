47e2618e52679a80529aceee0efbaec95f455dbe
Andrej Kirejeŭ <gs1994@gmail.com>
2018-04-18 19:18:01 +0300

    get rid of TS warnings

--- src/Application.ts
+++ src/Application.ts
@@ -1,4 +1,4 @@
-import {AConnection, AConnectionPool} from "gdmn-db";
+import {AConnection, AConnectionPool, DBStructure} from "gdmn-db";
 import {erExport, ERModel} from "gdmn-orm";
 import {Context, IDBDetail} from "./Context";
 
@@ -10,15 +10,23 @@ export class Application extends Context {
     const {poolInstance, poolOptions, connectionOptions}: IDBDetail = db;
     await poolInstance.create(connectionOptions, poolOptions);
 
-    const dbStructure = await AConnectionPool.executeConnection(poolInstance,
+    const result = await AConnectionPool.executeConnection(poolInstance,
       (connection) => AConnection.executeTransaction(connection, async (transaction) => {
-        return await transaction.readDBStructure();
+        console.time("time");
+        const dbStructure = await transaction.readDBStructure();
+        console.log("DBStructure loaded...");
+        console.timeEnd("time");
+        console.time("time");
+        const erModel = erExport(dbStructure, transaction, new ERModel());
+        console.log("erModel: loaded " + Object.entries(erModel.entities).length + " entities");
+        console.timeEnd("time");
+        return {
+          dbStructure,
+          erModel
+        };
       }));
 
-    const erModel = erExport(dbStructure, new ERModel());
-    console.log('erModel: loaded ' + Object.entries(erModel.entities).length + ' entities');
-
-    return new Application({dbDetail: db, dbStructure, erModel});
+    return new Application({...result, dbDetail: db});
   }
 
   public static async destroy(app: Application): Promise<boolean> {
